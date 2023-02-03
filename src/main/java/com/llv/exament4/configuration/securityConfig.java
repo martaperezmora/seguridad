99a804342a9deb9540613969d51a70385d075442
Marta Perez <mpermor2205@g.educaand.es>
2023-02-03 14:01:51 +0100

    añadido usuarios

--- src/main/java/com/llv/exament4/configuration/securityConfig.java
+++ src/main/java/com/llv/exament4/configuration/securityConfig.java
@@ -1,11 +1,49 @@
 package com.llv.exament4.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class securityConfig extends WebSecurityConfigurerAdapter{
-        
+public class securityConfig{
+    
+    @Bean
+    public UserDetailsService user(){
+
+        UserDetails user = User.builder()
+        .username("user")
+        .password("{noop}user*1234")
+        .authorities("USER")
+        .build();
+
+        UserDetails admin = User.builder()
+        .username("admin")
+        .password("{noop}admin*1234")
+        .authorities("USER","ADMIN")
+        .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests()
+            .anyRequest()     // cualquier peticion
+            .authenticated()  // debe autenticarse 
+        .and()
+            .formLogin()      // se usará un formulario
+        .and()
+            .httpBasic();  
+
+        return http.build();
+    }
+
 }
